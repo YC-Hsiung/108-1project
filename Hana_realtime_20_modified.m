f1603db1c894228bffe57285eea3b08e35319af0
as5845131421 <a1091372@gmail.com>
2019-11-18 23:13:29 +0800

    plot washing rectangle now

--- Hana_realtime_20_modified.m
+++ Hana_realtime_20_modified.m
@@ -5,14 +5,8 @@
 % modified date:2019/10/11
 % modified for 4 channel 20points assay
 clear;
-%pre_folder_list=dir(pwd);
-%folder_list={};
-%for i=1:length(pre_folder_list)
-%    if pre_folder_list(i).isdir==1 & i>2
-%        folder_list=[folder_list;pre_folder_list(i).name]
-%    end
-%end
-folder_list={'6 EDCNHS';'7 EDCNHS wash';'8 antibody 100 ug-ml';'9 antibody wash';'10 blocking';'11 blocking wash';'12 antigen secreted TNF';'13 antigen wash'};
+
+folder_list={'1 EDCNHS';'2 EDCNHS wash';'3 antibody';'4 antibody wash';'5 blocking';'6 blocking wash';'7 antigen';'8 antigen wash'};
 start_index=0
 steps=[]
 for j = 1:length(folder_list)
@@ -60,19 +54,41 @@ add_column=4;
 peak=[peak(:,1:5) zeros(size(peak,1),add_column) peak(:,6:10) zeros(size(peak,1),add_column) peak(:,11:15) zeros(size(peak,1),add_column) peak(:,16:20) zeros(size(peak,1),add_column)];%每個channel後面加三column
 first={'channe1','','','','','average','shift','STD','-offset','channe2','','','','','average','shift','STD','-offset','channe3','','','','','average','shift','STD','-offset','channe4','','','','','average','shift','STD','-offset'};
 line_format={'b-','g-','r-','k-'};
+%draw retangles
+WashingSection=[2,4,6,8];
+section_count=0;
+last_section_x=0;
+axis off;
+for i=1:size(steps,1)
+    min_y=-1;
+    max_y=5;
+    retangle_color=[153/255,204/255,255/255];
+    if steps(i)~=""
+        section_count=section_count+1;
+        if find(section_count-1==WashingSection)
+            rectangle('Position',[last_section_x+2,min_y+0.1,i*4-4-last_section_x-4,max_y-min_y],'EdgeColor', retangle_color, 'FaceColor', retangle_color, 'LineWidth', 5);
+        end
+        last_section_x=i*4-4;
+    end
+    if i==size(steps,1)
+        if find(section_count==WashingSection)
+            rectangle('Position',[last_section_x+2,min_y+0.1,i*4-4-last_section_x-4,max_y-min_y],'EdgeColor', retangle_color, 'FaceColor', retangle_color, 'LineWidth', 5);
+        end
+    end
+end
+axis on;
 for i=0:3
     peak(:,9*i+6)=sum(peak(:,(i*9+1):(i*9+5)),2)/5; %average
     peak(2:end,9*i+7)=peak(2:end,(i*9+6))-peak(1,(i*9+6)); %shift
-    peak(:,9*i+8)=std(peak(:,(i*9+1):(i*9+5)),0,2); %STD
+    peak(2:end,9*i+8)=std(peak(2:end,(i*9+1):(i*9+5))-peak(1,(i*9+1):(i*9+5)) , 0,2); %STD
     peak(:,9*i+9)=peak(:,9*i+7)-peak(2,9*i+7); %-offset
     %plot
-    time_x=0:size(peak,1)-1;
+    time_x=0:size(peak,1)-2;
     time_x=time_x*4;
     set(gcf,'Visible', 'off');
-    errorbar(time_x,peak(:,9*i+9),peak(:,9*i+8),char(line_format(i+1)),'DisplayName',['channel' char(i+49)]);
-    
-    
-    
+    legend_name={'10','1','5','40'};
+    %errorbar(time_x,peak(2:end,9*i+9),peak(2:end,9*i+8),char(line_format(i+1)),'DisplayName',['channel' char(i+49)]);
+    errorbar(time_x,peak(2:end,9*i+9),peak(2:end,9*i+8),char(line_format(i+1)),'DisplayName',[char(legend_name(i+1)) 'ug/ml']);
 end
 
 %plot adjustment
